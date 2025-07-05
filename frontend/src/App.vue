91c4e75daeeb8f10bb839c1be627938f0865d207
leuteritz <adrianleuteritz@web.de>
2025-07-06 00:29:00 +0200

    init

---
+++ frontend/src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { onMounted } from 'vue'
+
+onMounted(() => {
+  console.log('Bard Idle Game')
+})
+</script>
+
+<template>
+  <div class="flex items-center justify-center w-full h-screen bg-yellow-200">
+    <h1 class="font-bold text-center text-8xl">Bard Idle Game</h1>
+  </div>
+</template>
